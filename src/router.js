ad1813ec9a6f645b97f1c1d0e944cd39d32db8b5
Katrina Fuller <trinafuller@att.net>
2019-09-11 15:00:16 -0600

    connected House VIew to router

--- src/router.js
+++ src/router.js
@@ -40,6 +40,16 @@ export default new Router({
       path: '/houses',
       name: 'houses',
       component: Houses
+    },
+    {
+      path: '/houses/:houseId',
+      name: 'house',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: function () {
+        return import(/* webpackChunkName: "house" */ './views/House.vue')
+      }
     }
   ]
 })
