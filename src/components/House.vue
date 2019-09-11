1216ac5e9834a3b1cbb211977a73698caae62896
Katrina Fuller <trinafuller@att.net>
2019-09-11 14:50:04 -0600

    linked house to houses

---
+++ src/components/House.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="house"></div>
+</template>
+
+
+<script>
+export default {
+  name: "house",
+  props: ["houseProp"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
--- src/views/Houses.vue
+++ src/views/Houses.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="houses"></div>
+  <div class="houses container-fluid">
+    <div class="row">
+      <house v-for="house in houses" :houseProp="house" :key="house._id" />
+      <div class="col"></div>
+    </div>
+  </div>
 </template>
 
 
 <script>
+import House from "../components/House";
+
 export default {
   name: "houses",
+
   data() {
     return {};
   },
   mounted() {
     this.$store.dispatch("getHouses");
   },
-  computed: {},
+  computed: {
+    houses() {
+      return this.$store.state.houses;
+    }
+  },
   methods: {},
-  components: {}
+  components: { House }
 };
 </script>
 
