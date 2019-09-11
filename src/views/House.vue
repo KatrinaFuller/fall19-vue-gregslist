bf536f1ca7c53e6e89287d0dfc8972e93b8db74d
Katrina Fuller <trinafuller@att.net>
2019-09-11 16:40:42 -0600

    able to remove houses

--- src/views/House.vue
+++ src/views/House.vue
@@ -32,7 +32,7 @@ export default {
   },
   methods: {
     remove() {
-      this.$store.dispatch("deleteHouse", this.house._id);
+      this.$store.dispatch("removeHouse", this.house._id);
     }
   },
   components: {}
