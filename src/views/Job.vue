1d5b794fed652ceb5bb78dbc5a2cef9ba081af29
Katrina Fuller <trinafuller@att.net>
2019-09-11 19:23:12 -0600

    Jobs showing up

--- src/views/Job.vue
+++ src/views/Job.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="job"></div>
+  <div class="job">
+    Job Details
+    <h5>Company: {{job.company}}</h5>
+    <h5>Title: {{job.jobTitle}}</h5>
+    <h5>Rate: {{job.rate}}</h5>
+    <h5>Hours: {{job.hours}}</h5>
+    <h5>Description: {{job.description}}</h5>
+    <button class="btn btn-danger" @click="remove">Remove</button>
+  </div>
 </template>
 
 
@@ -9,8 +17,22 @@ export default {
   data() {
     return {};
   },
-  computed: {},
-  methods: {},
+  mounted() {
+    let payload = {
+      jobId: this.$route.params.jobId
+    };
+    this.$store.dispatch("getJobById", payload);
+  },
+  computed: {
+    job() {
+      return this.$store.state.activeJob;
+    }
+  },
+  methods: {
+    remove() {
+      this.$store.dispatch("removeJob", this.job._id);
+    }
+  },
   components: {}
 };
 </script>
